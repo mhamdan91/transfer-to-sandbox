b450b41ba21fc9ae82c18bc006d3814db21bde3b
Muhammad Hamdan <mhamdan.usa@gmail.com>
2021-08-17 18:32:12 -0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+    def _sleep(sleep_time: int) -> None:
+        while sleep_time > 0:
+            time.sleep(10)
+            sleep_time -= 10
+            msg = f'\r {sleep_time} remaining seconds before resuming auditing...'
+            sys.stdout.write(msg)
+            sys.stdout.flush()
+_sleep(60)
