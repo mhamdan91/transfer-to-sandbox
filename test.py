7cd9eac946961766c1dfb9d7c2ee89038ad63972
Muhammad Hamdan <mhamdan.usa@gmail.com>
2021-08-17 18:41:31 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,10 +3,11 @@ import logging
 log = logging.getLogger(__name__)
 def _sleep(sleep_time: int) -> None:
     while sleep_time > 0:
-        time.sleep(10)
+        time.sleep(1)
         sleep_time -= 10
 #         msg = f'\r {sleep_time} remaining seconds before resuming auditing...'
 #         sys.stdout.write(msg)
 #         sys.stdout.flush()
         log.info("%s remaining seconds before resuming auditing...", sleep_time)
+        print(f"{sleep_time} remaining seconds before resuming auditing......")
 _sleep(65)
